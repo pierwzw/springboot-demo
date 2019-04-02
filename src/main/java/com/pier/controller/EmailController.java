e729001587ab5eba4b52773c32866a15d013ad77
zhongweiwu <zhongweiwu@pptv.com>
2019-04-02 19:38:05 +0800

    integrated spring boot with email

---
+++ src/main/java/com/pier/controller/EmailController.java
@@ -0,0 +1,34 @@
+package com.pier.controller;
+
+import com.pier.mail.MailUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @auther zhongweiwu
+ * @date 2019/4/2 18:29
+ */
+@Slf4j
+@RestController
+public class EmailController {
+
+
+    @Autowired
+    private MailUtil mailUtil;
+
+    @GetMapping("/mail")
+    public String sendMail() {
+
+        try {
+            mailUtil.sendMail("2019773066@qq.com", "123", UUID.randomUUID().toString().substring(0, 6));
+            log.info("send mail successed");
+        } catch (Exception e) {
+            return "发送失败!";
+        }
+        return "发送成功!";
+    }
+}
---
+++ src/main/java/com/pier/mail/MailUtil.java
@@ -0,0 +1,51 @@
+package com.pier.mail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.internet.MimeMessage;
+
+/**
+ * @auther zhongweiwu
+ * @date 2019/4/2 18:25
+ */
+@Service
+public class MailUtil {
+
+    @Autowired
+    private JavaMailSender jms;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    public void sendMail(String toUser, String subject, String text) {
+
+        subject = "感谢您的注册~ ";
+
+        text = "感谢你的注册，你的默认密码是："+"<h3>"+text+"</h3>";
+
+        try {
+            MimeMessage mimeMessage = jms.createMimeMessage();
+            MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
+            // 设置发信人，发信人需要和spring.mail.username配置的一样否则报错
+            message.setFrom(username);
+            // 设置收信人
+            message.setTo(toUser);
+            // 设置主题
+            message.setSubject(subject);
+            // 第二个参数true表示使用HTML语言来编写邮件
+            message.setText(text, true);
+            //FileSystemResource file = new FileSystemResource(new File("src/main/resources/static/image/picture.jpg"));
+            //helper.addAttachment("图片.jpg", file);//添加带附件的邮件
+            //helper.addInline("picture", file);//添加带静态资源的邮件
+            jms.send(mimeMessage);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+
+        }
+    }
+}
